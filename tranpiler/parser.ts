6f297421a6884ce2602a61feb5f703ef94809578
Nguyen Bao <ngiabao508@gmail.com>
2021-11-25 19:11:50 +0700

    Con loop, double loop Type II, VD 2 type 1 not working, Trien khai bang cach nhap tu input dau vao

---
+++ tranpiler/parser.ts
@@ -0,0 +1,367 @@
+import {
+	Token,
+	Basic,
+	Delemiter,
+	LiTToken,
+	Operator,
+	DataType,
+	Keyword,
+	getAssociated,
+	Associated,
+	getPrecedence,
+	LoopType
+} from "@tranpiler/token";
+import {
+	AssignExpr,
+	BinaryExpr,
+	Expr,
+	IfElseExpr,
+	NestedLoopExpr,
+	MathExpr,
+	Operand,
+	UnaryExpr,
+	LoopParameter,
+	ArrayInjectorExpr
+} from "@tranpiler/expr";
+import { FunctionDecl, VariableIdentifier } from "@tranpiler/expr";
+
+export class Parser {
+	index: number = 0;
+	expr: Array<Expr> = [];
+	tokens: Array<Token> = [];
+	next() {
+		return this.tokens[++this.index];
+	}
+	reset() {
+		this.index = 0;
+		this.expr = [];
+		this.tokens = [];
+	}
+	private get peek() {
+		return this.tokens[this.index + 1];
+	}
+	private get current() {
+		return this.tokens[this.index];
+	}
+	parse(tokens: Array<Token>) {
+		this.tokens = tokens;
+		return this.statements();
+	}
+	statements() {
+		if (
+			this.current.Type == Basic.LITERAL &&
+			this.peek.Type == Delemiter.LPRAREN
+		) {
+			return this.functionParser();
+		}
+	}
+	private functionParser() {
+		const cur = this.current as LiTToken;
+		const fname = cur.Value;
+		const parameter = [];
+
+		// const return :
+		this.next(); // get (
+		// scan parameter ;
+		if (this.peek.Type != Delemiter.RPRAREN) {
+			while (this.current && this.current.Type != Delemiter.RPRAREN) {
+				let parname = (this.next() as LiTToken).Value;
+				this.next(); // skip to type
+				let partype = this.next().Type;
+				parameter.push(new VariableIdentifier(parname, partype as DataType));
+				if (this.next().Type == Delemiter.RPRAREN) {
+					continue; //continue to break;
+				}
+			}
+		} else {
+			this.next();
+		}
+
+		let retur: VariableIdentifier | undefined = undefined;
+
+		if (this.peek.Type != Keyword.PRE) {
+			// scan return ;
+			let parname = (this.next() as LiTToken).Value;
+			this.next(); // skip to type
+			let partype = this.next().Type;
+			retur = new VariableIdentifier(parname, partype as DataType);
+		}
+		const preidx = this.tokens.findIndex((value) => {
+			return value.Type == Keyword.PRE;
+		});
+		const postidx = this.tokens.findIndex((value) => {
+			return value.Type == Keyword.POST;
+		});
+		const eofidx = this.tokens.findIndex((value) => {
+			return value.Type == Basic.EOF;
+		});
+
+		let pretok = this.tokens.slice(preidx + 1, postidx);
+		let pre: MathExpr | undefined = this.parsePreExpr(pretok);
+		let posttok = this.tokens.slice(postidx + 1, eofidx);
+		let post = this.parsePostExpr(posttok);
+		return new FunctionDecl(fname, parameter, post, pre, retur);
+	}
+	parsePreExpr(tokens: Token[]): MathExpr | undefined {
+		return this.genASTTree(tokens);
+	}
+	genASTTree(tokens: Token[]): MathExpr | undefined {
+		const RPN = this.genRPN(tokens) as Operand[];
+		//RPN Shunting-yard algorithm from  https://en.wikipedia.org/wiki/Shunting-yard_algorithm :)) đọc mệt quá =(())
+		// https://aquarchitect.github.io/swift-algorithm-club/Shunting%20Yard/
+		let temp: Operand[] = new Array(0);
+		while (RPN.length > 0) {
+			const token = RPN.shift() as Token;
+			if (
+				RPN.length >= 3 &&
+				token.Type == Basic.LITERAL &&
+				(RPN[0] as Token).Type == Delemiter.LBRACK &&
+				(RPN[1] as Token).Type == Basic.LITERAL &&
+				(RPN[2] as Token).Type == Delemiter.RBRACK
+			) {
+				const aray = VariableIdentifier.fromLitoken(token as LiTToken);
+				temp.push(new ArrayInjectorExpr(aray, RPN[1] as LiTToken));
+				RPN.shift();
+				RPN.shift();
+				RPN.shift();
+				continue;
+			}
+
+			switch (token.Type) {
+				case Basic.LITERAL:
+					temp.push(token as LiTToken);
+					break;
+				case Operator.PLUS:
+				case Operator.MINUS:
+				case Operator.STAR:
+				case Operator.SLASH:
+				case Operator.PERCENT:
+				case Operator.GREATER:
+				case Operator.LESSER:
+				case Operator.EQUALS:
+				case Operator.NOT_EQUAL:
+				case Operator.GREATER_EQUAL:
+				case Operator.LESSER_EQUAL:
+				case Operator.AND:
+				case Operator.OR:
+					let right = temp.pop() as LiTToken | Expr;
+					let left = temp.pop() as LiTToken | Expr;
+					temp.push(new BinaryExpr(token, left, right));
+					break;
+				case Operator.NOT:
+					temp.push(new UnaryExpr(token, temp.pop() as LiTToken | Expr));
+					break;
+			}
+		}
+		return temp.pop() as MathExpr | undefined;
+	}
+	genRPN(token: Token[]) {
+		const operatorStack: Token[] = [];
+		const outputStack: Token[] = [];
+		token.forEach((token, index, arr) => {
+			if (
+				arr.length >= index + 1 + 3 &&
+				token.Type == Basic.LITERAL &&
+				(arr[index + 1] as Token).Type == Delemiter.LPRAREN &&
+				(arr[index + 2] as Token).Type == Basic.LITERAL &&
+				(arr[index + 3] as Token).Type == Delemiter.RPRAREN
+			) {
+				(arr[index + 1] as Token).Type = Delemiter.LBRACK;
+				(arr[index + 3] as Token).Type = Delemiter.RBRACK;
+			}
+
+			switch (token.Type) {
+				case Basic.LITERAL:
+				case Delemiter.LBRACK:
+				case Delemiter.RBRACK:
+					outputStack.push(token);
+					break;
+				case Operator.PLUS:
+				case Operator.MINUS:
+				case Operator.STAR:
+				case Operator.SLASH:
+				case Operator.PERCENT:
+				case Operator.GREATER:
+				case Operator.LESSER:
+				case Operator.EQUALS:
+				case Operator.NOT_EQUAL:
+				case Operator.GREATER_EQUAL:
+				case Operator.LESSER_EQUAL:
+				case Operator.AND:
+				case Operator.OR:
+					let top = operatorStack[operatorStack.length - 1]; // peek
+
+					if (
+						(top &&
+							getAssociated(token.Type) == Associated.LEFT &&
+							getPrecedence(token.Type) >=
+								getPrecedence(top.Type as Operator)) ||
+						(top &&
+							getAssociated(token.Type) == Associated.RIGHT &&
+							getPrecedence(token.Type) > getPrecedence(top?.Type as Operator))
+					) {
+						let y = operatorStack.pop();
+						outputStack.push(y as Token);
+					}
+					operatorStack.push(token);
+					break;
+
+				case Delemiter.LPRAREN:
+					operatorStack.push(token);
+					break;
+				case Delemiter.RPRAREN:
+					let t: Token | undefined;
+					while (true) {
+						t = operatorStack.pop();
+						if (t && (t as Token).Type != Delemiter.LPRAREN) {
+							outputStack.push(t as Token);
+						} else {
+							break;
+						}
+					}
+					break;
+			}
+		});
+		while (operatorStack.length > 0) {
+			outputStack.push(operatorStack.pop() as Token);
+		}
+
+		return outputStack;
+	}
+	genIfElse(
+		ast: Operand | undefined
+	): AssignExpr | IfElseExpr | Expr | undefined {
+		if (ast instanceof BinaryExpr && ast.Type == Operator.OR) {
+			const left = this.genIfElse(ast.left);
+			const right = this.genIfElse(ast.right);
+			let cur = left;
+			while (cur instanceof IfElseExpr && cur.Wrong) {
+				cur = cur.Wrong;
+			}
+			if (cur instanceof IfElseExpr && right instanceof Expr) {
+				cur.Wrong = right;
+			}
+			return left;
+			// TODO: Some bug was there #1
+		} else if (ast instanceof BinaryExpr && ast.Type == Operator.EQUALS) {
+			const t = VariableIdentifier.fromLitoken(ast.left as LiTToken);
+			const assign = new AssignExpr(t, ast.right);
+			return assign;
+		} else if (ast instanceof BinaryExpr && ast.Type == Operator.AND) {
+			if (ast.left instanceof BinaryExpr && ast.left.Type == Operator.EQUALS) {
+				const t = VariableIdentifier.fromLitoken(ast.left.left as LiTToken);
+				const assign = new AssignExpr(t, ast.left.right);
+				return new IfElseExpr(ast.right as MathExpr, assign);
+			}
+			if (ast.left instanceof BinaryExpr && ast.left.Type == Operator.AND) {
+				let start = ast;
+				let prev = start;
+				let cur = start;
+				while (
+					cur.left instanceof BinaryExpr &&
+					cur.left.Type != Operator.EQUALS
+				) {
+					prev = cur;
+					cur = cur.left;
+				}
+				prev.left = cur.right;
+				if (
+					cur.left instanceof BinaryExpr &&
+					cur.left.Type == Operator.EQUALS
+				) {
+					const t = VariableIdentifier.fromLitoken(cur.left.left as LiTToken);
+					const assign = new AssignExpr(t, cur.left.right);
+					return new IfElseExpr(start as MathExpr, assign);
+				}
+				return ast;
+			}
+			return ast;
+		}
+		return undefined;
+	}
+	parseLoop(tokens: Token[]) {
+		let index = 0;
+		function next() {
+			return tokens[++index];
+		}
+		function current() {
+			return tokens[index];
+		}
+		function peek() {
+			return tokens[index + 1];
+		}
+		let cur = current();
+		if (cur.Type == Basic.LITERAL) {
+			new VariableIdentifier((cur as LiTToken).Value);
+		}
+		let expr = new NestedLoopExpr();
+
+		next(); // skip =
+		next(); // skip (
+		const scanLoop = () => {
+			let parameter = new LoopParameter();
+			cur = next();
+			if (cur.Type == LoopType.VM || cur.Type == LoopType.TT) {
+				parameter.type = cur.Type;
+			}
+			cur = next();
+			if (cur.Type == Basic.LITERAL) {
+				parameter.identifier = VariableIdentifier.fromLitoken(
+					cur as LiTToken,
+					DataType.N
+				);
+			}
+			next(); // next TH
+			next(); // next {
+			cur = next();
+			if (cur.Type == Basic.LITERAL) {
+				// from value
+				parameter.from = cur as LiTToken;
+			}
+			next(); // next skip ..
+			let toToken: Token[] = [];
+			while (true) {
+				cur = next();
+				toToken.push(cur);
+				if (cur.Type == Delemiter.RBRACE) {
+					break;
+				}
+			}
+			parameter.to = this.genASTTree(toToken);
+			next(); //skip .
+			expr.parameter.push(parameter);
+		};
+		cur = peek();
+		scanLoop();
+		if (peek().Type == LoopType.VM || peek().Type == LoopType.TT) {
+			scanLoop();
+		}
+		let bodyToks: Token[] = [];
+		while (true) {
+			cur = next();
+			if (cur == undefined || cur.Type == Basic.EOF) {
+				break;
+			}
+			bodyToks.push(cur);
+		}
+		bodyToks.pop(); // pop last )
+		expr.body = this.genASTTree(bodyToks) as MathExpr;
+		return expr;
+	}
+	parsePostExpr(tokens: Token[]): Operand | undefined {
+		const isType2 = tokens.findIndex((value) => {
+			return value.Type == LoopType.TH;
+		});
+
+		if (isType2 < 0) {
+			//type 1
+			const ast: Operand | undefined = this.genASTTree(tokens);
+			const exprs = this.genIfElse(ast);
+			return exprs ? exprs : ast;
+		} else if (isType2 > 0) {
+			//type 2 no idea
+			const exprs = this.parseLoop(tokens);
+			return exprs ? exprs : this.genASTTree(tokens);
+		}
+	}
+}
---
+++ tranpiler/scanner.ts
@@ -0,0 +1,241 @@
+import {
+	Token,
+	LitKind,
+	Basic,
+	Delemiter,
+	LiTToken,
+	Keyword,
+	LoopType,
+	Operator,
+	DataType,
+	SpecialLITERAL
+} from "@tranpiler/token";
+import {
+	isAlphabet,
+	isBlank,
+	isDot,
+	isFloat,
+	isInt,
+	isNum,
+	isSign,
+	isStar
+} from "@tranpiler/utils";
+export class Scanner {
+	scan(src: string): Token[] {
+		let s = "";
+		let tokens: Token[] = [];
+		for (let i = 0; i < src.length; i++) {
+			const c = src[i];
+			const peek = src[i + 1] || " ";
+			if (isBlank(c) && isBlank(peek)) {
+				continue;
+			}
+			s = s.trimStart() + c;
+			if (
+				(s.length > 0 && isNum(s[0]) && !isNum(peek) && !isDot(peek)) ||
+				(s.length > 0 && isSign(s[0]) && !isNum(peek) && !isDot(peek)) ||
+				(i == src.length - 1 && isNum(s[0])) ||
+				isSign(s[0])
+			) {
+				//capture number
+				let number = Number.parseFloat(s.trim());
+				let hasDotDot = false;
+				if (s.endsWith("..")) {
+					hasDotDot = true;
+				}
+				if (isInt(number)) {
+					tokens.push(new LiTToken(number, LitKind.IntLit));
+					s = "";
+					if (hasDotDot) {
+						tokens.push(new Token(Delemiter.DOTDOT));
+					}
+					continue;
+				}
+				if (isFloat(number)) {
+					tokens.push(new LiTToken(number, LitKind.FloatLit));
+					s = "";
+
+					continue;
+				}
+			} else if (
+				(s.length > 0 &&
+					isAlphabet(s[0]) &&
+					!isAlphabet(peek) &&
+					!isNum(peek) &&
+					!isStar(peek)) ||
+				(i == src.length - 1 && isAlphabet(s[0]))
+			) {
+				//capture keyword && literal
+				switch (s.trimEnd()) {
+					case "TT":
+						tokens.push(new Token(LoopType.TT));
+						s = "";
+						continue;
+					case "TRUE":
+						tokens.push(new LiTToken(SpecialLITERAL.TRUE, LitKind.IntLit));
+						s = "";
+						continue;
+					case "FALSE":
+						tokens.push(new LiTToken(SpecialLITERAL.FALSE, LitKind.IntLit));
+						s = "";
+						continue;
+					case "VM":
+						tokens.push(new Token(LoopType.VM));
+						s = "";
+						continue;
+					case "TH":
+						tokens.push(new Token(LoopType.TH));
+						s = "";
+						continue;
+					case "pre":
+						tokens.push(new Token(Keyword.PRE));
+						s = "";
+						continue;
+					case "post":
+						tokens.push(new Token(Keyword.POST));
+						s = "";
+						continue;
+					case "N":
+						tokens.push(new Token(DataType.N));
+						s = "";
+						continue;
+					case "R":
+						tokens.push(new Token(DataType.R));
+						s = "";
+						continue;
+					case "Z":
+						tokens.push(new Token(DataType.Z));
+						s = "";
+						continue;
+					case "R*":
+						tokens.push(new Token(DataType.R_STAR));
+						s = "";
+						continue;
+					case "Z*":
+						tokens.push(new Token(DataType.Z_STAR));
+						s = "";
+						continue;
+					case "B":
+						tokens.push(new Token(DataType.B));
+						s = "";
+						continue;
+					case "char*":
+						tokens.push(new Token(DataType.CHAR_STAR));
+						s = "";
+						continue;
+				}
+				tokens.push(new LiTToken(s, LitKind.Unknown));
+				s = "";
+				continue;
+			} else if (s.length > 2 && s.startsWith('"') && s.endsWith('"')) {
+				tokens.push(new LiTToken(s.replaceAll('"', ""), LitKind.StringLit));
+				s = "";
+			} else if (
+				(s.length > 0 && isNum(s[0])) ||
+				(s.length > 0 && isAlphabet(s[0]) && isStar(c)) ||
+				(s == "<" && peek == "=") ||
+				(s == ">" && peek == "=") ||
+				(s == "!" && peek == "=") ||
+				(s == "&" && peek == "&") ||
+				(s == "|" && peek == "|")
+			) {
+				// skip if current char is dot and start with number
+				continue;
+			}
+			switch (s) {
+				//Delemiter
+				case "(":
+					tokens.push(new Token(Delemiter.LPRAREN));
+					s = "";
+					continue;
+				case "[":
+					tokens.push(new Token(Delemiter.LBRACK));
+					s = "";
+					continue;
+				case "{":
+					tokens.push(new Token(Delemiter.LBRACE));
+					s = "";
+					continue;
+				case ")":
+					tokens.push(new Token(Delemiter.RPRAREN));
+					s = "";
+					continue;
+				case "]":
+					tokens.push(new Token(Delemiter.RBRACK));
+					s = "";
+					continue;
+				case "}":
+					tokens.push(new Token(Delemiter.RBRACE));
+					s = "";
+					continue;
+				case ",":
+					tokens.push(new Token(Delemiter.COMMA));
+					s = "";
+					continue;
+				case ":":
+					tokens.push(new Token(Delemiter.COLON));
+					s = "";
+					continue;
+				case ".":
+					tokens.push(new Token(Delemiter.DOT));
+					s = "";
+					continue;
+				case "+":
+					tokens.push(new Token(Operator.PLUS));
+					s = "";
+					continue;
+				case "-":
+					tokens.push(new Token(Operator.MINUS));
+					s = "";
+					continue;
+				case "*":
+					tokens.push(new Token(Operator.STAR));
+					s = "";
+					continue;
+				case "%":
+					tokens.push(new Token(Operator.PERCENT));
+					s = "";
+					continue;
+				case ">":
+					tokens.push(new Token(Operator.GREATER));
+					s = "";
+					continue;
+				case "<":
+					tokens.push(new Token(Operator.LESSER));
+					s = "";
+					continue;
+				case "=":
+					tokens.push(new Token(Operator.EQUALS));
+					s = "";
+					continue;
+				case "!=":
+					tokens.push(new Token(Operator.NOT_EQUAL));
+					s = "";
+					continue;
+				case ">=":
+					tokens.push(new Token(Operator.GREATER_EQUAL));
+					s = "";
+					continue;
+				case "<=":
+					tokens.push(new Token(Operator.LESSER_EQUAL));
+					s = "";
+					continue;
+				case "/":
+					tokens.push(new Token(Operator.SLASH));
+					s = "";
+					continue;
+				case "&&":
+					tokens.push(new Token(Operator.AND));
+					s = "";
+					continue;
+				case "||":
+					tokens.push(new Token(Operator.OR));
+					s = "";
+					continue;
+				//Delemiter
+			}
+		}
+		tokens.push(new Token(Basic.EOF));
+		return tokens;
+	}
+}
